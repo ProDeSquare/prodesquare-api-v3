ef9e6caa54057b6406e71cfacf0dab36d8975967
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 01:48:16 +0500

    get featured projects

---
+++ src/controllers/featured.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import Featured from "../schemas/featured.schema";
+
+// @desc    get featured projects
+// @route   GET /api/v3/projects/featured
+// @access  Public
+export const getFeatured = async (req: Request, res: Response) => {
+  try {
+    const featured = await Featured.find().sort({ _id: -1 });
+
+    res.status(200).json(featured);
+  } catch (err) {
+    //
+  }
+};
