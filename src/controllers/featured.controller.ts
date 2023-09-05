635759702abba95bc22e6aaac26722a74ea28eda
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 02:50:01 +0500

    typed controllers

--- src/controllers/featured.controller.ts
+++ src/controllers/featured.controller.ts
@@ -4,7 +4,10 @@ import Featured from "../schemas/featured.schema";
 // @desc    get featured projects
 // @route   GET /api/v3/projects/featured
 // @access  Public
-export const getFeatured = async (req: Request, res: Response) => {
+export const getFeatured = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const featured = await Featured.find()
       .sort({ _id: -1 })
--- src/controllers/projects.controller.ts
+++ src/controllers/projects.controller.ts
@@ -4,7 +4,10 @@ import Project from "../schemas/project.schema";
 // @desc    get other projects
 // @route   GET /api/v3/projects/get
 // @access  Public
-export const getProjects = async (req: Request, res: Response) => {
+export const getProjects = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const projects = await Project.find()
       .sort({ _id: 1 })
