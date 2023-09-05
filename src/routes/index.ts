dfd1a361524ba376d6dd1fa7de4aa636eda8f264
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 02:59:16 +0500

    wrapped route prefixes in quotes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,8 @@ import SkillRoutes from "./skills.router";
 import { Router } from "express";
 
 const router: Router = Router();
-router.use(`/projects`, FeaturedRoutes);
+router.use("/projects", FeaturedRoutes);
 router.use("/projects", ProjectRouter);
-router.use(`/skills`, SkillRoutes);
+router.use("/skills", SkillRoutes);
 
 export default router;
