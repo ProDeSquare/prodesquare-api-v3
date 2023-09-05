46b4e0e72a4c48cb77b4270461ec23bb4167d67b
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 03:00:49 +0500

    removed mongodb url from log

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connection = async (): Promise<void> => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log("MongoDB connected");
   } catch (err) {
     console.log(`Error: ${err}`);
     process.exit(1);
