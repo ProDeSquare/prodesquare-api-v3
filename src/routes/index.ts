b9df461f623bcae8fdecedce53c59a28f963115f
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 02:18:48 +0500

    project routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import FeaturedRoutes from "./featured.router";
+import ProjectRouter from "./projects.router";
 import SkillRoutes from "./skills.router";
 import { Router } from "express";
 
 const router: Router = Router();
 router.use(`/projects`, FeaturedRoutes);
+router.use("/projects", ProjectRouter);
 router.use(`/skills`, SkillRoutes);
 
 export default router;
