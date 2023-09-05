a36d23988d3e7a7a3fbd870da85ca1adb402451a
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 03:07:22 +0500

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import connection from "./db/connection";
 import Routes from "./routes";
 import dotenv from "dotenv";
 import path from "path";
+import cors from "cors";
 
 dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
@@ -10,6 +11,7 @@ const server: Express = express();
 const PORT: number = +process.env.PORT || 8000;
 connection();
 
+server.use(cors());
 server.use(`${process.env.ROUTE_PREFIX}`, Routes);
 
 server.listen(PORT, (): void => {
