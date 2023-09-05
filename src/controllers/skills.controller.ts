098a1589697eecae0aa94a109f8bea37cfd66257
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 02:47:12 +0500

    skills accepting classification parameter

--- src/controllers/skills.controller.ts
+++ src/controllers/skills.controller.ts
@@ -4,9 +4,11 @@ import Skill from "../schemas/skill.schema";
 // @desc    get skills
 // @route   GET /api/v3/skills/get
 // @access  Public
-export const getSkills = async (req: Request, res: Response) => {
+export const getSkills = async (req: Request, res: Response): Promise<void> => {
   try {
-    const skills = await Skill.find();
+    const skills = req.params.classification
+      ? await Skill.find({ type: req.params.classification }).sort({ level: 1 })
+      : await Skill.find().sort({ level: 1 });
 
     res.status(200).json(skills);
   } catch (err) {
