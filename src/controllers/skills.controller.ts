bd801a2a4c38bfc9b24d8a6bc61f9691ce75b5af
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 03:55:03 +0500

    structuring skills

--- src/controllers/skills.controller.ts
+++ src/controllers/skills.controller.ts
@@ -1,14 +1,27 @@
 import { Request, Response } from "express";
 import Skill from "../schemas/skill.schema";
+import capitalize from "../helpers/capitalize.helper";
 
 // @desc    get skills
 // @route   GET /api/v3/skills/get
 // @access  Public
 export const getSkills = async (req: Request, res: Response): Promise<void> => {
   try {
-    const skills = req.params.classification
-      ? await Skill.find({ type: req.params.classification }).sort({ level: 1 })
+    const classification: string | undefined = capitalize(
+      req.params.classification
+    ).replace(/s$/, "");
+
+    const initial = classification
+      ? await Skill.find({ type: classification }).sort({ level: 1 })
       : await Skill.find().sort({ level: 1 });
+
+    const skills: Record<string, string[]> = {};
+
+    initial.forEach((skill) => {
+      skills[`${skill.type}s`]
+        ? skills[`${skill.type}s`].push(skill.technology)
+        : (skills[`${skill.type}s`] = []);
+    });
 
     res.status(200).json(skills);
   } catch (err) {
