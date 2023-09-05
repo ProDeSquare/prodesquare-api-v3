dd306ee8ef1ad299dfdfccc6d208c8f4709726ff
Hamza Mughal <hamza@prodesquare.com>
2023-09-06 01:58:16 +0500

    get featured projects route accept optional count parameter

--- src/routes/featured.router.ts
+++ src/routes/featured.router.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { getFeatured } from "../controllers/featured.controller";
 
 const FeaturedRoutes: Router = Router();
-FeaturedRoutes.get("/featured", getFeatured);
+FeaturedRoutes.get("/featured/:count?", getFeatured);
 
 export default FeaturedRoutes;
