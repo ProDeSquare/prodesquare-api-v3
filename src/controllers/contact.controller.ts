6ea549c99fd20a255551950ed318812963c0bcd2
Hamza Mughal <hamza@prodesquare.com>
2023-09-13 02:04:53 +0500

    send email controller

---
+++ src/controllers/contact.controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import nodemailer from "nodemailer";
+
+type MessageType = {
+  from: { name: string; address: string };
+  to: string;
+  subject: string;
+  text: string;
+  replyTo: string;
+};
+
+// @desc    Send Email
+// @route   GET /api/v3/contact/send-message
+// @access  Public
+export const sendMessage = (req: Request, res: Response): void => {
+  const { name, email, message }: Record<string, string> = req.body;
+
+  const transporter: nodemailer.Transporter = nodemailer.createTransport({
+    host: process.env.SMTP,
+    port: +process.env.SMTP_PORT,
+    secure: process.env.NODE_ENV === "production",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const msg: MessageType = {
+    from: {
+      name: name,
+      address: process.env.EMAIL,
+    },
+    to: process.env.MAIL_TO,
+    subject: "Contact Form Submission",
+    text: `${name} said: ${message}`,
+    replyTo: email,
+  };
+
+  transporter.sendMail(msg, (err: Error, info) => {
+    err
+      ? res.status(400).send("Something went wrong!")
+      : res.status(201).send("Mail sent successfully!");
+  });
+};
